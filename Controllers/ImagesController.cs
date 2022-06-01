f474ebd20428c8c53eef3b0aa60230243d085603
notdeshlan <u21481084@tuks.co.za>
2022-06-01 19:16:24 +0200

    Add project files.

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW03_u21481084.Controllers
+{
+    public class ImagesController : Controller
+    {
+        // GET: Images
+        public ActionResult Images()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW03_u21481084.MvcApplication" Language="C#" %>
